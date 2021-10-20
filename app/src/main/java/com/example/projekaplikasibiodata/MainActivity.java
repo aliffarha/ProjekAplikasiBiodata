5ca901696c35c958125338ac77978ed51ff015bd
aliffarha <alipfarha@gmail.com>
2021-10-21 00:30:48 +0700

    echo "# ProjekAplikasiBiodata" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/aliffarha/ProjekAplikasiBiodata.git git push -u origin main

--- app/src/main/java/com/example/projekaplikasibiodata/MainActivity.java
+++ app/src/main/java/com/example/projekaplikasibiodata/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.projekaplikasibiodata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,30 +24,12 @@ public class MainActivity extends AppCompatActivity {
         telepon = findViewById(R.id.telepon);
         email = findViewById(R.id.email);
 
-        alamat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gotoUrl("https://www.google.co.id/maps/place/Jl.+Pelem+Golek+II,+Tambakaji,+Kec.+Ngaliyan,+Kota+Semarang,+Jawa+Tengah+50185/@-6.9846816,110.3431026,15z/data=!3m1!4b1!4m5!3m4!1s0x2e708ab6600b7a59:0xc5b0bd03f29c9e84!8m2!3d-6.9846817!4d110.3518574");
-
-            }
-        });
+        alamat.setOnClickListener(view -> gotoUrl("https://www.google.co.id/maps/place/Jl.+Pelem+Golek+II,+Tambakaji,+Kec.+Ngaliyan,+Kota+Semarang,+Jawa+Tengah+50185/@-6.9846816,110.3431026,15z/data=!3m1!4b1!4m5!3m4!1s0x2e708ab6600b7a59:0xc5b0bd03f29c9e84!8m2!3d-6.9846817!4d110.3518574"));
 
 
 
-        telepon.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            gotoUrl("https://wa.me/6281804124208");
-
-        }
-    });
-        email.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gotoUrl("mailto:alipfarha@gmail.com");
-
-            }
-        });
+        telepon.setOnClickListener(view -> gotoUrl("https://wa.me/6281804124208"));
+        email.setOnClickListener(view -> gotoUrl("mailto:alipfarha@gmail.com"));
 
 
 }
